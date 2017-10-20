4509c3676d8dc7f3d8771592b5c690910f7075f2
Dmitry.Mashkov <Dmitry.Mashkov@billing.ru>
2017-10-20 09:28:55 +0300

    Included support for inline export

--- plugin.js
+++ plugin.js
@@ -1,10 +1,12 @@
-var DeclarationBundlerPlugin = (function () {
-    function DeclarationBundlerPlugin(options) {
+"use strict";
+var DtsBundlerPlugin = (function () {
+    function DtsBundlerPlugin(options) {
         if (options === void 0) { options = {}; }
-        this.out = options.out ? options.out : './build/';
+        this.out = options.out || './build/';
         this.excludedReferences = options.excludedReferences ? options.excludedReferences : undefined;
+        this.moduleName = options.moduleName || 'Lib';
     }
-    DeclarationBundlerPlugin.prototype.apply = function (compiler) {
+    DtsBundlerPlugin.prototype.apply = function (compiler) {
         var _this = this;
         //when the compiler is ready to emit files
         compiler.plugin('emit', function (compilation, callback) {
@@ -32,33 +34,40 @@ var DeclarationBundlerPlugin = (function () {
             callback();
         });
     };
-    DeclarationBundlerPlugin.prototype.generateCombinedDeclaration = function (declarationFiles) {
+    DtsBundlerPlugin.prototype.generateCombinedDeclaration = function (declarationFiles) {
         var declarations = '';
         for (var fileName in declarationFiles) {
             var declarationFile = declarationFiles[fileName];
-            var data = declarationFile.source();
-            var lines = data.split("\n");
+            var data = declarationFile._value;
+            var lines = data.split('\n');
             var i = lines.length;
             while (i--) {
                 var line = lines[i];
                 //exclude empty lines
-                var excludeLine = line == "";
+                var excludeLine = line == '';
                 //exclude export statements
-                excludeLine = excludeLine || line.indexOf("export") !== -1;
+                excludeLine = excludeLine || /export\s+({)|(default)/.test(line);
+                // leave a line with inline export but remove export keyword
+                if (!excludeLine) {
+                    lines[i] = lines[i].replace(/export\s/, '');
+                    lines[i] = lines[i].replace(/declare\s/, '');
+                    line = lines[i];
+                }
                 //exclude import statements
-                excludeLine = excludeLine || line.indexOf("import") !== -1;
+                excludeLine = excludeLine || line.indexOf('import') !== -1;
                 //if defined, check for excluded references
-                if (!excludeLine && this.excludedReferences && line.indexOf("<reference") !== -1) {
+                if (!excludeLine && this.excludedReferences && line.indexOf('<reference') !== -1) {
                     excludeLine = this.excludedReferences.some(function (reference) { return line.indexOf(reference) !== -1; });
                 }
                 if (excludeLine) {
                     lines.splice(i, 1);
                 }
             }
-            declarations += lines.join("\n") + "\n\n";
+            declarations += lines.join('\n') + '\n\n';
         }
-        return declarations;
+        var output = 'declare module ' + this.moduleName + '\n{\n' + declarations + '}';
+        return output;
     };
-    return DeclarationBundlerPlugin;
-})();
-module.exports = DeclarationBundlerPlugin;
+    return DtsBundlerPlugin;
+}());
+module.exports = DtsBundlerPlugin;
--- plugin.ts
+++ plugin.ts
@@ -1,89 +1,88 @@
-class DtsBundlerPlugin
-{
-	out:string;
-	moduleName:string;
-	mode:string;
+class DtsBundlerPlugin {
+  out: string;
+  moduleName: string;
+  excludedReferences: any[];
 
-	constructor(options:any={})
-	{
-		this.out = options.out ? options.out : './build/';
-		this.excludedReferences = options.excludedReferences ? options.excludedReferences : undefined;
-	}
+  constructor(options: any = {}) {
+    this.out = options.out || './build/';
+    this.excludedReferences = options.excludedReferences ? options.excludedReferences : undefined;
+    this.moduleName = options.moduleName || 'Lib';
+  }
 
-	apply(compiler)
-	{
-		//when the compiler is ready to emit files
-		compiler.plugin('emit', (compilation,callback) =>
-		{
-			//collect all generated declaration files
-			//and remove them from the assets that will be emited
-			var declarationFiles = {};
-			for (var filename in compilation.assets)
-			{
-				if(filename.indexOf('.d.ts') !== -1)
-				{
-					declarationFiles[filename] = compilation.assets[filename];
-					delete compilation.assets[filename];
-				}
-			}
+  apply(compiler) {
+    //when the compiler is ready to emit files
+    compiler.plugin('emit', (compilation, callback) => {
+      //collect all generated declaration files
+      //and remove them from the assets that will be emited
+      var declarationFiles = {};
+      for (var filename in compilation.assets) {
+        if (filename.indexOf('.d.ts') !== -1) {
+          declarationFiles[filename] = compilation.assets[filename];
+          delete compilation.assets[filename];
+        }
+      }
 
-			//combine them into one declaration file
-			var combinedDeclaration = this.generateCombinedDeclaration(declarationFiles);
+      //combine them into one declaration file
+      var combinedDeclaration = this.generateCombinedDeclaration(declarationFiles);
 
-			//and insert that back into the assets
-			compilation.assets[this.out] = {
-				source: function() {
-					return combinedDeclaration;
-				},
-				size: function() {
-					return combinedDeclaration.length;
-				}
-			};
+      //and insert that back into the assets
+      compilation.assets[this.out] = {
+        source: function () {
+          return combinedDeclaration;
+        },
+        size: function () {
+          return combinedDeclaration.length;
+        }
+      };
 
-			//webpack may continue now
-			callback();
-		});
-	}
+      //webpack may continue now
+      callback();
+    });
+  }
 
-	private generateCombinedDeclaration(declarationFiles:Object):string
-	{
-		var declarations = '';
-		for(var fileName in declarationFiles)
-		{
-			var declarationFile = declarationFiles[fileName];
-			var data = declarationFile._value;
+  private generateCombinedDeclaration(declarationFiles: Object): string {
+    var declarations = '';
+    for (var fileName in declarationFiles) {
+      var declarationFile = declarationFiles[fileName];
+      var data = declarationFile._value;
 
-			var lines = data.split("\n");
-			var i = lines.length;
+      var lines = data.split('\n');
+      var i = lines.length;
 
 
-			while (i--)
-			{
-				var line = lines[i];
-				
-				//exclude empty lines
-				var excludeLine = line == "";
-				
-				//exclude export statements
-				excludeLine = excludeLine || line.indexOf("export") !== -1;
-				
-				//exclude import statements
-				excludeLine = excludeLine || line.indexOf("import") !== -1;
-				
-				//if defined, check for excluded references
-				if (!excludeLine && this.excludedReferences && line.indexOf("<reference") !== -1) {
-					excludeLine = this.excludedReferences.some(reference => line.indexOf(reference) !== -1);
-				}
-				if (excludeLine) {
-					lines.splice(i, 1);
-				}
-			}
-			declarations += lines.join("\n") + "\n\n";
-		}
+      while (i--) {
+        var line = lines[i];
 
-		var output = "declare module "+this.moduleName+"\n{\n" + declarations + "}";
-		return output;
-	}
+        //exclude empty lines
+        var excludeLine = line == '';
+
+        //exclude export statements
+        excludeLine = excludeLine || /export\s+({)|(default)/.test(line);
+
+        // leave a line with inline export but remove export keyword
+        if (!excludeLine) {
+          lines[i] = lines[i].replace(/export\s/, '');
+          lines[i] = lines[i].replace(/declare\s/, '');
+          line = lines[i];
+        }
+
+        //exclude import statements
+        excludeLine = excludeLine || line.indexOf('import') !== -1;
+
+        //if defined, check for excluded references
+        if (!excludeLine && this.excludedReferences && line.indexOf('<reference') !== -1) {
+          excludeLine = this.excludedReferences.some(reference => line.indexOf(reference) !== -1);
+        }
+        if (excludeLine) {
+          lines.splice(i, 1);
+        }
+      }
+      declarations += lines.join('\n') + '\n\n';
+    }
+
+    var output = 'declare module ' + this.moduleName + '\n{\n' + declarations + '}';
+    return output;
+  }
 
 }
 
